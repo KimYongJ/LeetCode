12ff4d999426057a88472e5ee78ba0a69dd7ceb7
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-04-09 16:16:35 +0900

    Time: 6 ms (49.69%), Space: 42.4 MB (90.01%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,28 @@
+class Solution {
+    static HashMap<Character, Integer> hm = new HashMap<>(){
+        {
+            put('I',1);
+            put('V',5);
+            put('X',10);
+            put('L',50);
+            put('C',100);
+            put('D',500);
+            put('M',1000);  
+        }  
+    };
+    
+    public int romanToInt(String s) {
+        int sum = 0;
+        char[] crr = s.toCharArray();
+        for(int i=0; i<crr.length; i++){
+            if(i+1!=crr.length && hm.get(crr[i])<hm.get(crr[i+1])){
+                sum += hm.get(crr[i+1])-hm.get(crr[i++]);
+            }
+            else{
+                sum += hm.get(crr[i]);
+                
+            }
+        }
+        return sum;
+    }
+}
